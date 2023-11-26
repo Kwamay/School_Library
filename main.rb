5168a80dcea21ef2e289ffeeb95abb9728f623aa
Kenneth Adutwum <kwamayadu@gmail.com>
2023-11-25 19:26:09 -0600

    Update file main, person and teacher .rb

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ def display_menu
   puts '2 - List all people'
   puts '3 - Create a person'
   puts '4 - Create a book'
-  puts '5 - Create a rental'
+  puts '5 - Create a rentals'
   puts '6 - List all rentals for a given person id'
   puts '7 - Exit'
 end
